2dd192de6989d1152ec8dce1fff0f2021fa64106
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2022-07-04 14:55:25 +0400

    minor change for initial

--- src/main/kotlin/Example.kt
+++ src/main/kotlin/Example.kt
@@ -1,14 +1,14 @@
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
-class Calculator {
+class Example {
     fun sum(a: Int, b: Int) = a + b
 }
 
-class CalculatorTest {
+class ExampleTest {
     @Test
     fun testSum(){
-        val calc = Calculator()
+        val calc = Example()
 
         Assertions.assertThat(calc.sum(11,10)).isEqualTo(21)
     }
